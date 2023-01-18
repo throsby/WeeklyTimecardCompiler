ce961f57f1c6b31759997e84e2e62e832c950c54
Throsby <throsbywells@gmail.com>
2023-01-17 20:41:20 -0500

    update

--- weeklyTimecard.py
+++ weeklyTimecard.py
@@ -1,28 +1,24 @@
 # Most of this is taken whole-cloth from "https://stackoverflow.com/questions/1180115/add-text-to-existing-pdf-using-python"
 def createBoxRental():
-    from PyPDF2 import PdfFileWriter, PdfFileReader
-    import io
-    from reportlab.pdfgen import canvas
-    from reportlab.lib.pagesizes import letter
-    from privateVariables import private_key
-
-
-    weekendingDate = "January 21, 2023"
-
+    
+    
 
     packet = io.BytesIO()
     can = canvas.Canvas(packet, pagesize=letter)
 
-    can.grid([x for x in range(700) if x % 20==0],[x for x in range(850) if x % 20==0])
-    
-
-    can.drawString(100, 100, ". (100, 100)")
+    # can.grid([x for x in range(700) if x % 20==0],[x for x in range(850) if x % 20==0])
+    # can.grid([x+1 for x in range(700) if x % 100==0],[x+1 for x in range(850) if x % 100==0])
 
     # Location for Employee name
     can.drawString(135, 645, "Throsby Wells")
     # Location for special string
     can.drawString(425, 645, private_key)
 
+    # Location for the weekly box rental rate
+    can.drawString(150, 568, "$15/week")
+
+    # Location for signature date
+    can.drawString(360, 175, weekendingDate)
 
     # Location for Week ending date
     can.drawString(175, 530, weekendingDate)
@@ -43,4 +39,16 @@ def createBoxRental():
     outputStream.close()
 
 if __name__ == "__main__":
+    from PyPDF2 import PdfFileWriter, PdfFileReader
+    import io
+    from reportlab.pdfgen import canvas
+    from reportlab.lib.pagesizes import letter
+    from privateVariables import private_key
+    from datetime import date
+    weekendingDate = "January 21, 2023"
+
+    thisSaturdayAsOrdinal = date.fromordinal(date.today().toordinal() + (5 - date.today().weekday()))
+    print(thisSaturdayAsOrdinal)
+    weekendingDateTimeFormat = thisSaturdayAsOrdinal.strftime("%B %w, %Y")
+    print("WeekendingFormat", weekendingDateTimeFormat)
     createBoxRental()
