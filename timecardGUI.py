aeb7c1f1a874ccaf6390f9630a8a24325d3f54bf
Throsby <throsbywells@gmail.com>
2023-01-19 14:31:19 -0500

    chugging along on adding labels and StringVars for hours worked

--- timecardGUI.py
+++ timecardGUI.py
@@ -9,35 +9,62 @@ class App(tk.Frame):
         self.entrythingy = tk.Entry()
         self.entrythingy.pack()
 
+        self.contents = tk.StringVar()
+        self.contents.set("this is a variable")
+        
+        self.entrythingy["textvariable"] = self.contents
+        self.entrythingy.bind('<Key-Return>', self.print_contents)
+
+        # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
         self.weekendingDateAsDatetime = tk.Entry()
         self.weekendingDateAsDatetime.pack()
         self.weekendingDate = tk.StringVar()
         self.weekendingDate.set(wT.weekendingDate)
 
-        self.but = tk.Button(root,text="Press Me!", command=self.helloCallback)
+        self.but = tk.Button(master,text="Press Me!", command=self.helloCallback)
         self.but.pack()
 
         self.weekendingDateAsDatetime["textvariable"] = self.weekendingDate
+
+        for pair in wT.weeklyHoursAsDateTime:
+            self.dayWorkedFrame = tk.Frame(master)
+            self.dayWorked = tk.Label(self.dayWorkedFrame, text=pair[1])
+            self.dayWorked.pack()
+            self.dayWorkedFrame.pack()
+
+            self.entry = tk.Entry(self.dayWorkedFrame)
+            self.entry["width"] = 20
+            self.entry.pack()
+
+            self.hours = tk.StringVar()
+            self.hours.set(pair[0])
+            self.entry["textvariable"] = self.hours
+            # self.hours.pack()
+            # print(pair)
+
         
-        self.contents = tk.StringVar()
-        
-        self.contents.set("this is a variable")
-
-        self.entrythingy["textvariable"] = self.contents
-
-        self.entrythingy.bind('<Key-Return>', self.print_contents)
-
     def print_contents(self, event):
         print("Hi. The current event content is:", self.contents.get())
 
     def helloCallback(self):
-        self.contents.set(wT.weekendingDate)
-        print("Button pressed!")
+        if (self.contents.get() == wT.weekendingDate): 
+            self.but["text"] = "Press Me!"
+            self.contents.set("Undone!")
+            print("Button pressed!")
+        else: 
+            self.contents.set(wT.weekendingDate)
+            self.but["text"] = "Change back"
+            print("Button pressed!")
+
+
+    
 
 root = tk.Tk()
 myapp = App(root)
 
+
 myapp.master.title("TimeCard Completer")
-myapp.master.maxsize(200,200)
+myapp.master.maxsize(1000,1000)
 
 myapp.mainloop()
