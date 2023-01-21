55fed353b93265797451626cd547efa7243ff4ff
Throsby <throsbywells@gmail.com>
2023-01-20 23:41:25 -0500

    making json version of weeklyTimecard

--- weeklyTimecard.py
+++ weeklyTimecard.py
@@ -8,12 +8,13 @@ from reportlab.lib.pagesizes import letter
 from privateVariables import private_key
 from datetime import datetime, date, timedelta, time
 
+import json
+
 # Create a value that is always the coming Saturday
 thisSaturdayAsDatetime = date.fromordinal(date.today().toordinal() + (5 - date.today().weekday()))
 
 boxrentalFilenameString = ""
 timecardFilenameString = ""
-
 
 # Formats this Saturday as "Month DD, YYYY"
 weekendingDate = thisSaturdayAsDatetime.strftime("%B %d, %Y")
@@ -25,7 +26,6 @@ wednesdayHours = 12
 thursdayHours = 12
 fridayHours = 12
 saturdayHours = 0
-
 
 # For clearer code to fix in a month or so, these are the datetimes of the week, as Datetimes
 sundayAsDatetime = thisSaturdayAsDatetime - timedelta(days=6)
@@ -45,16 +45,24 @@ thursday = thisSaturdayAsDatetime.day - 2
 friday = thisSaturdayAsDatetime.day - 1
 saturday = thisSaturdayAsDatetime.day - 0
 
+workWeekAsDict = {
+    "sunday": {
+        "Datetime": "{}".format(sundayAsDatetime),
+        
+    }
+}
+
+workWeekAsJson = ''
+
 weeklyHoursAsDateTime = [(sundayHours, sundayAsDatetime), (mondayHours, mondayAsDatetime), (tuesdayHours, tuesdayAsDatetime), (wednesdayHours, wednesdayAsDatetime), (thursdayHours, thursdayAsDatetime), (fridayHours, fridayAsDatetime), (saturdayHours, saturdayAsDatetime)]
+weeklyHoursAsDateTimeAsJson = [(sundayHoursAsJson, sundayAsDatetime), (mondayHoursAsJson, mondayAsDatetime), (tuesdayHoursAsJson, tuesdayAsDatetime), (wednesdayHoursAsJson, wednesdayAsDatetime), (thursdayHoursAsJson, thursdayAsDatetime), (fridayHoursAsJson, fridayAsDatetime), (saturdayHoursAsJson, saturdayAsDatetime)]
+
 
 def createBoxRental():
     print("Creating Box Rental for {}".format(weekendingDate))
     global boxrentalFilenameString
     packet = io.BytesIO()
     can = canvas.Canvas(packet, pagesize=letter)
-
-    # can.grid([x for x in range(700) if x % 20==0],[x for x in range(850) if x % 20==0])
-    # can.grid([x+1 for x in range(700) if x % 100==0],[x+1 for x in range(850) if x % 100==0])
 
     # Location for Employee name
     can.drawString(135, 645, "Throsby Wells")
@@ -151,6 +159,63 @@ def createTimeCard():
     output.write(outputStream)
     outputStream.close()
 
+def createTimeCard(workWeekAsJson):
+    global timecardFilenameString
+    print("Creating TimeCard for {}".format(weekendingDate))
+    packet = io.BytesIO()
+    can = canvas.Canvas(packet, pagesize=letter)
+
+    # Location of name
+    can.drawString(30, 505, "Throsby Wells")
+    # Location of private key
+    can.drawString(290, 505, private_key)
+    # Location of guaranteed hours
+    can.drawString(455, 530, "12")
+    # Location of guaranteed rate
+    can.drawString(525, 530, "21.42/hour")
+    # Location of weekending
+    can.drawString(615, 530, weekendingDate)
+    # Location of job title
+    can.drawString(455, 505, "HS Coordinator")
+    # Location of meal allowance
+    can.drawString(90, 192, "$15/day")
+
+
+    daysOfWeekWorked = 0
+    for pair in weeklyHoursAsDateTime:
+        if (pair[0] != 0):
+            # Creates the different lines for times and dates
+            can.setFontSize(12)
+            can.drawString(23, 430 - daysOfWeekWorked, "NY   NY")
+            can.setFontSize(10)
+            can.drawString(120, 430 - daysOfWeekWorked, "{0}/{1}/{2}".format(pair[1].month, pair[1].day, pair[1].year))
+            # The endtime of work. Starting at 1:30PM to make lunch easier - adds the time worked for the respective day as time delta and combines the day worked with the hours worked
+            endOfWorkAsDatetime = datetime.combine(pair[1], time(13, 30)) + timedelta(seconds=pair[0]*60*60)
+            # print(datetime.strftime(endOfWorkAsDatetime, "%-H:%M%p"))
+
+            can.drawString(191, 430 - daysOfWeekWorked, "1P     7P    7:30  {hours}".format(hours=datetime.strftime(endOfWorkAsDatetime, "%-H:%M%p")))
+            daysOfWeekWorked = daysOfWeekWorked + 23
+
+    can.save()
+
+    # create a new PDF with Reportlab
+    new_pdf = PdfFileReader(packet)
+
+    # read your existing PDF
+    existing_pdf = PdfFileReader(open(r"/Users/throsbywells/Desktop/TimeCards/BlankTimeCard.pdf", "rb"))
+    output = PdfFileWriter()
+    # add the "watermark" (which is the new pdf) on the existing page
+    page = existing_pdf.getPage(0)
+    page.mergePage(new_pdf.getPage(0))
+    output.addPage(page)
+
+    # finally, write "output" to a real file
+    timecardFilenameString = datetime.strftime(thisSaturdayAsDatetime, "/Users/throsbywells/Desktop/TimeCards/Timecard_-_Throsby_Wells_-_x%-m-%d-%Y.pdf")
+    outputStream = open(timecardFilenameString, "wb")
+    output.write(outputStream)
+    outputStream.close()
+
+
 def mergeTCBR():
     output = PdfFileWriter()
     timecard = PdfFileReader(open(timecardFilenameString,"rb"))
@@ -168,6 +233,6 @@ def mergeTCBR():
     print("Merging files done!")
 
 if __name__ == "__main__":
-    createTimeCard()
-    createBoxRental()
-    mergeTCBR()
+    createTimeCard(workWeekAsJson)
+    # createBoxRental()
+    # mergeTCBR()
